628ddcccb27924ccbbf6cfeaf283a2e8c61d6859
Peter Munro <peter@petermunro.org>
2022-12-02 13:49:50 +0000

    completes exercise 2

--- src/components/CarTool.tsx
+++ src/components/CarTool.tsx
@@ -1,8 +1,40 @@
 function CarTool() {
   return (
-    <header>
-      <h1>Car Tool</h1>
-    </header>
+    <div>
+      <header>
+        <h1>Car Tool</h1>
+      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Make</th>
+            <th>Model</th>
+            <th>Year</th>
+            <th>Color</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>1</th>
+            <th>Maserati</th>
+            <th>Merak</th>
+            <th>2012</th>
+            <th>Blue</th>
+            <th>34000</th>
+          </tr>
+          <tr>
+            <th>2</th>
+            <th>Lamborghini</th>
+            <th>Countache</th>
+            <th>2017</th>
+            <th>Red</th>
+            <th>57000</th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
 
