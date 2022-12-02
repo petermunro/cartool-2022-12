4755e148c4e312259ddf8a5f44f9a086fef2e01a
Peter Munro <peter@petermunro.org>
2022-12-02 14:05:20 +0000

    completes exercise 4

--- src/components/CarTool.tsx
+++ src/components/CarTool.tsx
@@ -1,4 +1,15 @@
+import { useState } from "react";
+import "./CarTool.css";
+
 function CarTool() {
+  let [carForm, setCarForm] = useState({
+    make: "",
+    model: "",
+    year: 2012,
+    color: "",
+    price: 0,
+  });
+
   let cars = [
     {
       id: 1,
@@ -17,6 +28,15 @@ function CarTool() {
       price: 57000,
     },
   ];
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    let newCar = {
+      ...carForm,
+      [e.target.name]: e.target.value,
+    };
+    setCarForm(newCar);
+    console.log(newCar);
+  }
 
   return (
     <div>
@@ -47,6 +67,60 @@ function CarTool() {
           ))}
         </tbody>
       </table>
+      <form className="car-form">
+        <ul>
+          <li>
+            <label htmlFor="make">make:</label>
+            <input
+              type="text"
+              name="make"
+              id="make"
+              value={carForm.make}
+              onChange={handleChange}
+            />
+          </li>
+          <li>
+            <label htmlFor="model">model:</label>
+            <input
+              type="text"
+              name="model"
+              id="model"
+              value={carForm.model}
+              onChange={handleChange}
+            />
+          </li>
+          <li>
+            <label htmlFor="year">year:</label>
+            <input
+              type="text"
+              name="year"
+              id="year"
+              value={carForm.year}
+              onChange={handleChange}
+            />
+          </li>
+          <li>
+            <label htmlFor="color">color:</label>
+            <input
+              type="text"
+              name="color"
+              id="color"
+              value={carForm.color}
+              onChange={handleChange}
+            />
+          </li>
+          <li>
+            <label htmlFor="price">price:</label>
+            <input
+              type="text"
+              name="price"
+              id="price"
+              value={carForm.price}
+              onChange={handleChange}
+            />
+          </li>
+        </ul>
+      </form>
     </div>
   );
 }
